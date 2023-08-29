c232853426c7fab096b14fdc451cf560ea13c8de
cristianadiana.petcu <cristianadiana.petcu@gmail.com>
2023-08-29 17:58:34 +0300

    [2] Created users table, model, seeder and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,31 +10,15 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'nume'         => fake()->lastName(),
+            'prenume'      => fake()->firstName(),
+            'email'        => fake()->unique()->safeEmail(),
+            'parola'       => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'cnp'          => fake()->unique()->numerify('#############'),
+            'data_nastere' => fake()->date()
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
---
+++ database/migrations/2023_08_29_140150_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nume');
+            $table->string('prenume');
+            $table->string('email')->unique();
+            $table->string('parola');
+            $table->char('cnp', 13)->unique();
+            $table->date('data_nastere');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
